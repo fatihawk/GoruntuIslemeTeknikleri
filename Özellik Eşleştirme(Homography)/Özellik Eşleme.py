2bc971512683b11b3602b8b3b8def79f7b938c5b
FATIHSAHIN1 <fatihawk@gmail.com>
2019-06-22 17:43:20 +0300

    Add files via upload

---
+++ Özellik Eşleştirme(Homography)/Özellik Eşleme.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+img1= cv2.imread('ataturk.jpg',0)
+img2 = cv2.imread('ataturk1.jpg',0)
+
+orb = cv2.ORB_create()
+
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+matches = bf.match(des1,des2)
+matches = sorted(matches, key = lambda x:x.distance)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None, flags=2)
+plt.imshow(img3)
+plt.show()
